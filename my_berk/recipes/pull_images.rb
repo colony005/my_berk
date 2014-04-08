48f244be74fefa1e6ffe24c3fee7793d2fcc3ead
colony005 <thecolony005@gmail.com>
2014-04-08 16:10:11 -0700

    Update pull_images.rb

--- my_berk/recipes/pull_images.rb
+++ my_berk/recipes/pull_images.rb
@@ -1,6 +1,6 @@
 # Pull each of our defined apps
 node[:my_apps].each do |name, image|  
-  script "pull_app_#{name}_shykes/helloflask" do
+  script "pull_app_#{name}_image" do
     interpreter "bash"
     user "root"
     code <<-EOH
@@ -13,7 +13,7 @@ script "pull_nginx_image" do
   interpreter "bash"
   user "root"
   code <<-EOH
-    docker pull #{node[:dockerfile/nginx]}
+    docker pull #{node[:my_nginx]}
   EOH
 end
 
